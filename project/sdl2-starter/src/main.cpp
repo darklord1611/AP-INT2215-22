5423fd95c6dc43688f9b82fc748149d9d87b1c2f
Darklord1611 <tavietanh16112004@gmail.com>
2023-02-15 10:12:52 +0700

    Create a square that can move and bounce

--- project/sdl2-starter/src/main.cpp
+++ project/sdl2-starter/src/main.cpp
@@ -1,62 +1,145 @@
 #include<iostream>
 #include<SDL2/SDL.h>
-
-using namespace std;
-// Graphics
-const int WINDOW_WIDTH = 700;
-const int WINDOW_HEIGHT = 1200;
-
-SDL_Window* g_main_window;
-SDL_Renderer* g_main_renderer;
+#define SDL_MAIN_HANDLED
 
 // Colors
 namespace Colors {
     const SDL_Color BLACK = {0, 0, 0, SDL_ALPHA_OPAQUE};
+    const SDL_Color GREEN = {0, 255, 0, SDL_ALPHA_OPAQUE};
+    
 }
 
-static bool Init() {
-    if (SDL_Init(SDL_INIT_EVERYTHING) > 0) {
+using namespace std;
+// Graphics
+namespace Graphics {
+    struct Screen {
+        const int WIDTH = 1200;
+        const int HEIGHT = 600;
+        const int center_x = WIDTH / 2;
+        const int center_y = HEIGHT / 2;
+    };
+};
+
+// Game objects
+
+enum SquareState {IDLE, MOVING_UP, MOVING_DOWN, MOVING_RIGHT, MOVING_LEFT};
+struct Square 
+{
+    int HEIGHT;
+    int WIDTH;
+    int x;
+    int y;
+    SquareState state;
+    int step;
+};
+
+// Application
+struct App {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    Graphics:: Screen screen;
+    const int DEFAULT_STEP = 2;
+    Square square = {32, 32, screen.center_x, screen.center_y, IDLE, DEFAULT_STEP};
+
+} app;
+
+// SDL rountines
+
+bool InitSDL() 
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) > 0) 
+    {
         cout << "SDL_Init failed with error: " << SDL_GetError() << endl;
         return false;
     }
-    g_main_window = SDL_CreateWindow(
-        "SDL Create Window",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        WINDOW_HEIGHT,
-        WINDOW_WIDTH,
-        SDL_WINDOW_OPENGL
-    );
-    if(g_main_window == nullptr) {
-        cout << "Unable to open main window: " << SDL_GetError() << endl;
-        SDL_Quit();
-        return false;
-    }
-    g_main_renderer = SDL_CreateRenderer(g_main_window, -1, SDL_RENDERER_PRESENTVSYNC);
     return true;
 }
 
-void Shutdown() {
-    if(g_main_window != nullptr) {
-        SDL_DestroyWindow(g_main_window);
-        g_main_window = nullptr;
+// Graphic rountines
+void ClearScreen(SDL_Renderer* renderer) {
+    SDL_SetRenderDrawColor(renderer, Colors::BLACK.r, Colors::BLACK.g, Colors::BLACK.b, Colors::BLACK.a);
+    SDL_RenderClear(renderer);
+}
+
+void drawSquare(int x, int y) 
+{
+    SDL_Rect rect;
+    rect.x = x - (app.square.WIDTH / 2);
+    rect.y = y - (app.square.HEIGHT / 2);
+    rect.w = app.square.WIDTH;
+    rect.h = app.square.HEIGHT;
+    // Define the green color on the renderer
+    SDL_SetRenderDrawColor(app.renderer, Colors::GREEN.r, Colors::GREEN.g, Colors::GREEN.b, Colors::GREEN.a);
+    // Fill the triangle with green color
+    SDL_RenderFillRect(app.renderer, &rect);
+}
+
+// Application routines
+
+void setNewState(SquareState new_state) 
+{
+    app.square.state = new_state;
+}
+
+void increaseStep()
+{
+    app.square.step *= app.DEFAULT_STEP;
+
+}
+
+void decreaseStep() 
+{
+    int new_step = app.square.step / app.DEFAULT_STEP;
+    if(new_step > app.DEFAULT_STEP) 
+    {
+        app.square.step = new_step;
+    } else 
+    {
+        app.square.step = app.DEFAULT_STEP;
     }
-    if(g_main_window != nullptr) {
-        SDL_DestroyRenderer(g_main_renderer);
-        g_main_renderer = nullptr;
+}
+
+void ShutdownApplication() {
+    if(app.window != nullptr) {
+        SDL_DestroyWindow(app.window);
+        app.window = nullptr;
+    }
+    if(app.window != nullptr) {
+        SDL_DestroyRenderer(app.renderer);
+        app.renderer = nullptr;
     }
 
     SDL_Quit();
 }
 
-static void ClearScreen(SDL_Renderer* renderer) {
-    SDL_SetRenderDrawColor(renderer, Colors::BLACK.r, Colors::BLACK.g, Colors::BLACK.b, Colors::BLACK.a);
-    SDL_RenderClear(renderer);
+bool InitApplication() {
+    if(InitSDL() == false) 
+    {
+        ShutdownApplication();
+        return false;
+    }
+    app.window = SDL_CreateWindow(
+        "SDL Create Window",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        app.screen.WIDTH,
+        app.screen.HEIGHT,
+        SDL_WINDOW_OPENGL
+    );
+    if(app.window == nullptr) {
+        cout << "Unable to open main window: " << SDL_GetError() << endl;
+        SDL_Quit();
+        return false;
+    }
+    app.renderer = SDL_CreateRenderer(app.window, -1, SDL_RENDERER_PRESENTVSYNC);
+    return true;
 }
 
-int main() {
-    if(Init() == false) {
-        Shutdown();
+int main(int argc, char* argv[]) {
+    if(InitApplication() == false) 
+    {
+        ShutdownApplication();
+        return 1;
     }
 
     // Draw loop
@@ -65,7 +148,64 @@ int main() {
 
     while(running) 
     {
-        ClearScreen(g_main_renderer);
+        ClearScreen(app.renderer);
+
+        // Check square's state
+        switch(app.square.state) {
+            case MOVING_LEFT:
+            {
+                int new_top_left_x = app.square.x - app.square.step - (app.square.WIDTH / 2);
+                if(new_top_left_x > app.square.step) 
+                {
+                    app.square.x = app.square.x - app.square.step;
+                } else 
+                {
+                    app.square.x = (app.square.WIDTH / 2);
+                    setNewState(MOVING_RIGHT);
+                }
+                break;
+            }
+            case MOVING_UP:
+            {
+                int new_y = app.square.y - app.square.step;
+                if(new_y > (app.square.HEIGHT / 2)) 
+                {
+                    app.square.y = new_y;
+                } else 
+                {
+                    app.square.y = app.square.HEIGHT / 2;
+                    setNewState(MOVING_DOWN);
+                }
+                break;
+            }
+            case MOVING_DOWN:
+            {
+                int new_y = app.square.y + app.square.step;
+                if(new_y + (app.square.HEIGHT / 2) < app.screen.HEIGHT) 
+                {
+                    app.square.y = new_y;
+                } else 
+                {
+                    app.square.y = new_y - (new_y + (app.square.HEIGHT / 2) - app.screen.HEIGHT);
+                    setNewState(MOVING_UP);
+                }
+                break;
+            }
+            case MOVING_RIGHT:
+            {
+                int new_top_left_x = app.square.x + app.square.step;
+                if(new_top_left_x + (app.square.WIDTH / 2) <= app.screen.WIDTH)
+                {
+                    app.square.x = app.square.x + app.square.step;
+                } else 
+                {
+                    setNewState(MOVING_LEFT);
+                }
+                break;
+            }
+            default:
+                break;
+        }
 
         // Check and process I/O events
         if(SDL_PollEvent(&event)) 
@@ -75,9 +215,38 @@ int main() {
                 case SDL_KEYDOWN: 
                 {
                     running = event.key.keysym.scancode != SDL_SCANCODE_ESCAPE;
+                    if(event.key.keysym.scancode == SDL_SCANCODE_UP) 
+                    {
+                        setNewState(MOVING_UP);
+                    } 
+                    if(event.key.keysym.scancode == SDL_SCANCODE_DOWN) 
+                    {
+                        setNewState(MOVING_DOWN);
+                    }
+                    if(event.key.keysym.scancode == SDL_SCANCODE_RIGHT) 
+                    {
+                        setNewState(MOVING_RIGHT);
+                    }
+                    if(event.key.keysym.scancode == SDL_SCANCODE_LEFT) 
+                    {
+                        setNewState(MOVING_LEFT);
+                    }
+                    if(event.key.keysym.scancode == SDL_SCANCODE_S) 
+                    {
+                        setNewState(IDLE);
+                    }
+                    if(event.key.keysym.scancode == SDL_SCANCODE_I) 
+                    {
+                        increaseStep();
+                    }
+                    if(event.key.keysym.scancode == SDL_SCANCODE_D) 
+                    {
+                        decreaseStep();
+                    }
                     break;
                 }
-                case SDL_QUIT: {
+                case SDL_QUIT: 
+                {
                     running = false;
                     break;
                 }
@@ -85,9 +254,13 @@ int main() {
                     break;
             }
         }
+        
+        //Draw the new content on the renderer
+        drawSquare(app.square.x, app.square.y);
+        
         // Update the screen with the content rendered in the background buffer
-        SDL_RenderPresent(g_main_renderer);
+        SDL_RenderPresent(app.renderer);
     }
-    Shutdown();
+    ShutdownApplication();
     return 0;
 }
